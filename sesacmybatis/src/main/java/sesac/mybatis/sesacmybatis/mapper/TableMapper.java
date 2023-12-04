720d03a69712c7a67023379710f359d25ea49eeb
pipi-shortstocking <jannie435@naver.com>
2023-12-04 17:16:44 +0900

    practice: update, delete 제외 create, read 완성

---
+++ sesacmybatis/src/main/java/sesac/mybatis/sesacmybatis/mapper/TableMapper.java
@@ -0,0 +1,18 @@
+package sesac.mybatis.sesacmybatis.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import sesac.mybatis.sesacmybatis.domain.Table;
+
+import java.util.List;
+
+
+@Mapper
+public interface TableMapper {
+    List<Table> showAll();
+
+    @Insert("insert into `table`(title, content, writer) values (#{title}, #{content},#{writer})")
+    void InsertOne(Table table);
+
+    List<Table> updateOne();
+}
---
+++ sesacmybatis/src/main/java/sesac/mybatis/sesacmybatis/service/TableService.java
@@ -0,0 +1,62 @@
+package sesac.mybatis.sesacmybatis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sesac.mybatis.sesacmybatis.domain.Table;
+import sesac.mybatis.sesacmybatis.dto.TableDTO;
+import sesac.mybatis.sesacmybatis.mapper.TableMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TableService {
+    @Autowired
+    TableMapper tableMapper;
+
+    public List<TableDTO> getTableList() {
+        List<Table> table = tableMapper.showAll();
+        List<TableDTO> result = new ArrayList<>();
+
+        for(Table one : table) {
+            String id = Integer.toString(one.getId());
+
+            TableDTO tabledto = TableDTO.builder()
+                    .no(one.getWriter() + id)
+                    .id(one.getId())
+                    .title(one.getTitle())
+                    .content(one.getContent())
+                    .writer(one.getWriter())
+                    .registered(one.getRegistered())
+                    .build();
+
+            result.add(tabledto);
+        }
+            return result;
+    }
+
+    public void insertOne(Table table) {
+        tableMapper.InsertOne(table);
+    }
+
+    public List<TableDTO> updateTableList() {
+        List<Table> table = tableMapper.updateOne();
+        List<TableDTO> result = new ArrayList<>();
+
+        for(Table one : table) {
+            String id = Integer.toString(one.getId());
+
+            TableDTO tabledto = TableDTO.builder()
+                    .no(one.getWriter() + id)
+                    .id(one.getId())
+                    .title(one.getTitle())
+                    .content(one.getContent())
+                    .writer(one.getWriter())
+                    .registered(one.getRegistered())
+                    .build();
+
+            result.add(tabledto);
+        }
+        return result;
+    }
+}
