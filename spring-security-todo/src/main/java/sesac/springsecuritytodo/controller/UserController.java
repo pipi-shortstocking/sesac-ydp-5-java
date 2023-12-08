acae7fdf4f15a0359d720dad958366f6c7bcc9e5
pipi-shortstocking <jannie435@naver.com>
2023-12-08 18:48:51 +0900

    login token 개발

---
+++ spring-security-todo/src/main/java/sesac/springsecuritytodo/controller/UserController.java
@@ -0,0 +1,84 @@
+package sesac.springsecuritytodo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sesac.springsecuritytodo.dto.ResponseDTO;
+import sesac.springsecuritytodo.dto.UserDTO;
+import sesac.springsecuritytodo.entity.UserEntity;
+import sesac.springsecuritytodo.security.TokenProvider;
+import sesac.springsecuritytodo.service.UserService;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    // [After] jwt 적용
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO dto) {
+        try {
+            // req body에 담긴 사용자 정보를 이용해 저장할 사용자 엔티티 객체 생성
+            UserEntity user = UserEntity.builder()
+                    .email(dto.getEmail())
+                    .username(dto.getUsername())
+                    .password(dto.getPassword())
+                    .build();
+
+            // 서비스를 이용해서 레포지토리에 사용자를 저장
+            UserEntity registeredUser = service.create(user);
+
+            // 엔티티를 dto 변환
+            UserDTO resDTO = UserDTO.builder()
+                    .email(registeredUser.getEmail())
+                    .id(registeredUser.getId())
+                    .username(registeredUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(resDTO);
+        }
+        catch(Exception e) {
+            ResponseDTO resDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(resDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO dto) {
+        UserEntity user = service.getByCredentials(dto.getEmail(), dto.getPassword());
+
+        if(user != null) {
+            // 이메일, 비번으로 찾은 유저 있음 = 로그인 성공
+            // [before] jwt 적용
+//            final UserDTO resUserDTO = UserDTO.builder()
+//                    .email(user.getEmail())
+//                    .id(user.getId())
+//                    .build();
+
+            // [after] jwt 적용
+            final String token = tokenProvider.create(user);
+            final UserDTO resUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .id(user.getId())
+                    .token(token) // jwt 토큰 생성
+                    .build();
+
+            return ResponseEntity.ok().body(resUserDTO);
+        } else {
+            ResponseDTO resDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+
+            return ResponseEntity.badRequest().body(resDTO);
+        }
+    }
+}
